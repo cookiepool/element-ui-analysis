8d49215321e93adf072f2ab989375c449b17333a
lihao <lihao@cmii.chinamobile.com>
2021-03-16 16:17:28 +0800

    update: new comment

--- element-ui-code/examples/entry.js
+++ element-ui-code/examples/entry.js
@@ -1,20 +1,31 @@
+/***
+ * 官方网站的打包运行入口，等同于我们平时写的main.js（vue项目默认是main.js）
+ * ***/
 import Vue from 'vue';
 import entry from './app';
 import VueRouter from 'vue-router';
+// 这里配置了alias别名，所以可以写main/index.js，具体alias配置见build/config.js
 import Element from 'main/index.js';
+// 可以让web上的代码能高亮显示
 import hljs from 'highlight.js';
+// 引入路由配置
 import routes from './route.config';
+// 封装的官网相关的组件
 import demoBlock from './components/demo-block';
 import MainFooter from './components/footer';
 import MainHeader from './components/header';
 import SideNav from './components/side-nav';
 import FooterNav from './components/footer-nav';
+// 官网首页的多语言配置
 import title from './i18n/title';
 
+// 引入所有的样式
 import 'packages/theme-chalk/src/index.scss';
+
 import './demo-styles/index.scss';
 import './assets/styles/common.css';
 import './assets/styles/fonts/style.css';
+// 引入所有的icon
 import icon from './icon.json';
 
 Vue.use(Element);
@@ -60,6 +71,7 @@ router.afterEach(route => {
     }
   }
   document.title = 'Element';
+  // 谷歌的分析工具，详细引入脚本的信息见index.tpl
   ga('send', 'event', 'PageView', route.name);
 });
 
